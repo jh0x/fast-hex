d6f2ff2bc12a0c8df596decdf5ae33f649b20f7e
Joanna Hulboj <j@hulboj.org>
2025-11-18 19:56:59 +0000

    Improve the roundtrip fuzzer test

--- test/fuzz/fuzz_roundtrip.cpp
+++ test/fuzz/fuzz_roundtrip.cpp
@@ -9,25 +9,44 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t * Data, size_t Size)
 {
-    std::vector<uint8_t> encoded;
-    std::vector<uint8_t> decoded;
+    const std::size_t encoded_size = 2 * Size;
+    const std::size_t decoded_size = Size;
 
-    const std::size_t encodedSize = 2 * Size;
-    const std::size_t decodedSize = Size;
-
-    encoded.resize(encodedSize);
-    decoded.resize(decodedSize);
-
-    auto encodedPtr = reinterpret_cast<uint8_t *>(encoded.data());
-    auto decodedPtr = reinterpret_cast<uint8_t *>(decoded.data());
-
-    encodeHex(encodedPtr, Data, Size);
-    decodeHexLUT(decodedPtr, encodedPtr, decodedSize);
-
-    if (std::memcmp(Data, decodedPtr, decodedSize) != 0)
+    auto decode = [&](const auto & source, auto decode_func)
     {
-        assert(false && "Decoded data does not match original data");
-    }
+        std::vector<uint8_t> decoded(decoded_size);
+        decode_func(decoded.data(), source.data(), decoded_size);
+        assert(std::memcmp(Data, decoded.data(), decoded_size) == 0);
+    };
+
+    auto test_encode_scalar = [&]()
+    {
+        std::vector<uint8_t> encoded(encoded_size);
+        encodeHex(encoded.data(), Data, Size);
+
+        decode(encoded, decodeHexLUT);
+        decode(encoded, decodeHexLUT4);
+#if defined(__AVX2__)
+        decode(encoded, decodeHexVec);
+#endif
+    };
+
+#if defined(__AVX2__)
+    auto test_encode_vec = [&]()
+    {
+        std::vector<uint8_t> encoded(encoded_size);
+
+        encodeHexVec(encoded.data(), Data, Size);
+        decode(encoded, decodeHexLUT);
+        decode(encoded, decodeHexLUT4);
+        decode(encoded, decodeHexVec);
+    };
+#endif
+
+    test_encode_scalar();
+#if defined(__AVX2__)
+    test_encode_vec();
+#endif
 
     return 0;
 }
