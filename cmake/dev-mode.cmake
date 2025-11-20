1f99b34d4e0430d13ce9f0f329af25cead23de42
Joanna Hulboj <j@hulboj.org>
2025-11-20 20:05:41 +0000

    Rudimentary benchmark support

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -15,6 +15,50 @@ if(ENABLE_COVERAGE)
     include(cmake/coverage.cmake)
 endif()
 
+option(ENABLE_BENCHMARK "Enable benchmark support" OFF)
+if(ENABLE_BENCHMARK)
+    include(ExternalProject)
+
+    ExternalProject_Add(
+        google_benchmark
+        SOURCE_DIR ${CMAKE_SOURCE_DIR}/contrib/benchmark
+        BINARY_DIR ${CMAKE_BINARY_DIR}/benchmark-build
+        INSTALL_DIR ${CMAKE_BINARY_DIR}/benchmark-install
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_BUILD_TYPE=Release
+            -DBENCHMARK_ENABLE_TESTING=OFF -DBENCHMARK_ENABLE_GTEST_TESTS=OFF
+            -DBENCHMARK_ENABLE_EXAMPLES=OFF -DBENCHMARK_INSTALL_DOCS=OFF
+        BUILD_ALWAYS OFF
+        INSTALL_COMMAND ""
+    )
+
+    add_library(benchmark::benchmark STATIC IMPORTED GLOBAL) # Add GLOBAL here
+    add_library(benchmark::benchmark_main STATIC IMPORTED GLOBAL) # Add GLOBAL here
+
+    add_dependencies(benchmark::benchmark google_benchmark)
+    add_dependencies(benchmark::benchmark_main google_benchmark)
+
+    # Set the imported locations
+    set_target_properties(
+        benchmark::benchmark
+        PROPERTIES
+            IMPORTED_LOCATION
+                ${CMAKE_BINARY_DIR}/benchmark-build/src/libbenchmark.a
+            INTERFACE_INCLUDE_DIRECTORIES
+                ${CMAKE_SOURCE_DIR}/contrib/benchmark/include
+    )
+
+    set_target_properties(
+        benchmark::benchmark_main
+        PROPERTIES
+            IMPORTED_LOCATION
+                ${CMAKE_BINARY_DIR}/benchmark-build/src/libbenchmark_main.a
+            INTERFACE_INCLUDE_DIRECTORIES
+                ${CMAKE_SOURCE_DIR}/contrib/benchmark/include
+            INTERFACE_LINK_LIBRARIES benchmark::benchmark
+    )
+endif()
+
 include(cmake/lint-targets.cmake)
 include(cmake/spell-targets.cmake)
 
---
+++ test/benchmark/fast_hex_bench.cpp
@@ -0,0 +1,97 @@
+#include <fast_hex/fast_hex.hpp>
+
+#include <benchmark/benchmark.h>
+
+#include <cstring>
+#include <vector>
+
+std::vector<uint8_t> createBinaryData(size_t size)
+{
+    std::vector<uint8_t> data(size);
+    for (size_t i = 0; i < size; ++i)
+    {
+        data[i] = static_cast<uint8_t>(i % 256);
+    }
+    return data;
+}
+
+std::vector<uint8_t> createHexData(size_t binarySize)
+{
+    auto binary = createBinaryData(binarySize);
+    std::vector<uint8_t> hex(binarySize * 2);
+    encodeHex(hex.data(), binary.data(), binarySize);
+    return hex;
+}
+
+#define DEFINE_ENCODE_BENCHMARK(func_name, size_val, size_name) \
+    static void BM_##func_name##_##size_name(benchmark::State & state) \
+    { \
+        auto data = createBinaryData(size_val); \
+        std::vector<uint8_t> hex(size_val * 2); \
+\
+        for (auto _ : state) \
+        { \
+            func_name(hex.data(), data.data(), size_val); \
+            benchmark::DoNotOptimize(hex); \
+        } \
+    } \
+    BENCHMARK(BM_##func_name##_##size_name);
+
+#define DEFINE_DECODE_BENCHMARK(func_name, size_val, size_name) \
+    static void BM_##func_name##_##size_name(benchmark::State & state) \
+    { \
+        auto hex = createHexData(size_val); \
+        std::vector<uint8_t> binary(size_val); \
+\
+        for (auto _ : state) \
+        { \
+            func_name(binary.data(), hex.data(), size_val); \
+            benchmark::DoNotOptimize(binary); \
+        } \
+    } \
+    BENCHMARK(BM_##func_name##_##size_name);
+
+// ---- Encoding Benchmarks ----
+
+DEFINE_ENCODE_BENCHMARK(encodeHex, 8, 8B)
+DEFINE_ENCODE_BENCHMARK(encodeHex, 16, 16B)
+DEFINE_ENCODE_BENCHMARK(encodeHex, 32, 32B)
+DEFINE_ENCODE_BENCHMARK(encodeHex, 64, 64B)
+DEFINE_ENCODE_BENCHMARK(encodeHex, 1024, 1KB)
+DEFINE_ENCODE_BENCHMARK(encodeHex, 1024 * 1024, 1MB)
+
+#if defined(__AVX2__)
+DEFINE_ENCODE_BENCHMARK(encodeHexVec, 8, 8B)
+DEFINE_ENCODE_BENCHMARK(encodeHexVec, 16, 16B)
+DEFINE_ENCODE_BENCHMARK(encodeHexVec, 32, 32B)
+DEFINE_ENCODE_BENCHMARK(encodeHexVec, 64, 64B)
+DEFINE_ENCODE_BENCHMARK(encodeHexVec, 1024, 1KB)
+DEFINE_ENCODE_BENCHMARK(encodeHexVec, 1024 * 1024, 1MB)
+#endif // defined(__AVX2__)
+
+// ---- Decoding Benchmarks ----
+
+DEFINE_DECODE_BENCHMARK(decodeHexLUT, 8, 8B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT, 16, 16B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT, 32, 32B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT, 64, 64B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT, 1024, 1KB)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT, 1024 * 1024, 1MB)
+
+DEFINE_DECODE_BENCHMARK(decodeHexLUT4, 8, 8B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT4, 16, 16B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT4, 32, 32B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT4, 64, 64B)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT4, 1024, 1KB)
+DEFINE_DECODE_BENCHMARK(decodeHexLUT4, 1024 * 1024, 1MB)
+
+#if defined(__AVX2__)
+DEFINE_DECODE_BENCHMARK(decodeHexVec, 8, 8B)
+DEFINE_DECODE_BENCHMARK(decodeHexVec, 16, 16B)
+DEFINE_DECODE_BENCHMARK(decodeHexVec, 32, 32B)
+DEFINE_DECODE_BENCHMARK(decodeHexVec, 64, 64B)
+DEFINE_DECODE_BENCHMARK(decodeHexVec, 1024, 1KB)
+DEFINE_DECODE_BENCHMARK(decodeHexVec, 1024 * 1024, 1MB)
+#endif // defined(__AVX2__)
+
+BENCHMARK_MAIN();
